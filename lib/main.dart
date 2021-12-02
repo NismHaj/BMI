6fcb95e580b2b5fd5df4b675bc9efa429985d87f
NismHaj <nsoom1999@gmail.com>
2021-12-03 02:16:05 +0300

    6&7pages&change

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,21 @@
-import 'package:bmi_app/ui/pages/new_record.dart';
+import 'package:bmi_app/ui/pages/splash_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() {
-  runApp(MaterialApp(home: MyApp()));
+  runApp(MaterialApp(
+    home: ScreenUtilInit(
+      builder: () {
+        return MyApp();
+      },
+      designSize: const Size(390, 844),
+    ),
+  ));
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return AddRecord();
+    return SplashPage();
   }
 }
